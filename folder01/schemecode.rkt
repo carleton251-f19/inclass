391bc0fa4b3b633d1626c31ba70d68ac51152254
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-09-18 10:59:57 -0500

    Scripted auto-commit on change (2019-09-18 10:59:57) by gitwatch.sh

--- folder01/schemecode.rkt
+++ folder01/schemecode.rkt
@@ -1,2 +1,37 @@
+;; anonymous function, gets cleaned up immediately
 (lambda (x)
   (+ x 1))
+
+(define mynumber 6)
+
+(define add-one
+  (lambda (x)
+    (+ x 1)))
+
+(add-one 7)
+
+(     (lambda (x) (* x 2))    15)
+
+;; length of a list: recursion!!! yes!
+(define list-length
+  (lambda (lst)
+    (if (null? lst)
+        0
+        (+ 1 (list-length (cdr lst))))))
+
+(list-length '())
+(list-length '(a b c))
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
