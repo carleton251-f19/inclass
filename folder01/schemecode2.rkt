e87e1c09f2d40855963ff9237ed5611f4f32b4c3
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-09-20 10:42:13 -0500

    Scripted auto-commit on change (2019-09-20 10:42:13) by gitwatch.sh

--- folder01/schemecode2.rkt
+++ folder01/schemecode2.rkt
@@ -11,4 +11,28 @@
 (list-of-numbers? '(3 5))
 (list-of-numbers? '(3 a))
 
+; (remove-first 'a '(b a c a))  --> '(b c a)
+(define remove-first
+  (lambda (s lst)
+    (cond ( (null? lst) '() )  ;; base case
+          ( (equal? s (car lst)) (cdr lst) ) ;; first item is it
+          ( else (cons (car lst) (remove-first s (cdr lst)))))))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         
