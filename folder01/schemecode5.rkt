f1e028f017f068e418a717045180011c02272806
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-09-23 10:55:28 -0500

    Scripted auto-commit on change (2019-09-23 10:55:28) by gitwatch.sh

--- folder01/schemecode5.rkt
+++ folder01/schemecode5.rkt
@@ -1,11 +1,12 @@
 (define cons-each
   (lambda (s lst)
-    ........
+    (if (null? lst)
+        '()
+        (cons 
+         (cons s (car lst))
+         (cons-each s (cdr lst))))))
 
-
-
-    ))
-
+(cons-each 1 '( () (2) (4) (3)))
 
 
 (define subsets
