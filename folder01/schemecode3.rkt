6a8fce59af4750203ec6576e626e63c68a4c59a2
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-09-23 10:17:34 -0500

    Scripted auto-commit on change (2019-09-23 10:17:34) by gitwatch.sh

--- folder01/schemecode3.rkt
+++ folder01/schemecode3.rkt
@@ -2,7 +2,12 @@
 (define subst
   (lambda (old new input)
     (cond ((null? input) '())
-          ((list? input)  .............)
+
+          ((list? input)
+           (cons 
+            (subst old new (car input))
+            (subst old new (cdr input))))
+
           ((equal? old input) new)
           (else input))))
 
