39f1ca1b1c54a026ec9a0056ea305997d7ccf61b
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-09-25 09:56:01 -0500

    Scripted auto-commit on change (2019-09-25 09:56:01) by gitwatch.sh

--- folder01/schemecode6.rkt
+++ folder01/schemecode6.rkt
@@ -4,3 +4,15 @@
     (* a b)))
 
 (mult 3 5)
+
+;; Currying: a function of one parameter that returns
+;; another function, that takes another parameter
+;; (repeat as needed)
+(define multc
+  (lambda (a)
+    ;; what does it return... dumdumdum! another function!
+    (lambda (b)
+      (* a b))))
+
+
+    
