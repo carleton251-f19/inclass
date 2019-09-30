020779c8c7a4309f4174f0cee02ea3d6ec8eb905
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-09-30 10:44:44 -0500

    Scripted auto-commit on change (2019-09-30 10:44:44) by gitwatch.sh

--- folder2/pointersliding.c
+++ folder2/pointersliding.c
@@ -3,7 +3,7 @@
 int main() {
    int a = 12;
    int *x;
-   printf("It points here? %p\n", x);
+   //printf("It points here? %p\n", x);
    //x = (int *)2; // memory location 2(????!!)
    //*x = 42;
    *x = 3;
