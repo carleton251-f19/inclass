20c6de3cdda1b401f27aee1931e18d54acdcaf35
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-10-02 10:19:22 -0500

    Scripted auto-commit on change (2019-10-02 10:19:22) by gitwatch.sh

--- folder2/stack-vs-heap.c
+++ folder2/stack-vs-heap.c
@@ -6,7 +6,6 @@ double fun1(int x) {
     return x * 0.5;
 }
 
-
 double fun2(int *y) {
     *y = *y * 2;
     return *y * 1.5;
@@ -18,7 +17,6 @@ int main() {
     double a = fun1(x);
     printf("x = %i\n",x);
     printf("a = %g\n",a);
-
     int *z = malloc(sizeof(int));
     *z = 8;
     printf("z = %i\n",*z);
