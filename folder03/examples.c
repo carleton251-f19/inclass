441567b18132b017aee430b70d800943a3e1f321
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-10-09 10:01:07 -0500

    Scripted auto-commit on change (2019-10-09 10:01:07) by gitwatch.sh

--- folder03/examples.c
+++ folder03/examples.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
 int main() {
+   // The type of nums is int[]
    int nums[3];
    nums[0] = 11;
    nums[1] = 12;
    printf("a value %i\n",nums[0]);
+
+   // C lets you think about an array
+   // name as a pointer
+   printf("a value %i\n",*nums);
 }
