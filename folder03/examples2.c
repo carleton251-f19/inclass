d29504fa4bc60aacbbb127b25be6d9001f3c1d24
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-10-09 10:25:48 -0500

    Scripted auto-commit on change (2019-10-09 10:25:48) by gitwatch.sh

--- folder03/examples2.c
+++ folder03/examples2.c
@@ -2,32 +2,16 @@
 #include <stdlib.h>
 
 int main() {
-   // The type of nums is int[]
-   int nums[3];
-   nums[0] = 11;
-   nums[1] = 12;
-   printf("a value %i\n",nums[0]);
-
-   // C lets you think about an array
-   // name as a pointer
-   printf("a value %i\n",*nums);
-   printf("a value %i\n",*(nums+1));
-   printf("a value %p\n",nums);
-   printf("a value %p\n",nums+1);
-   int i = 3+5;
-   nums[i] = 19;
-   printf("a value %i\n",nums[i]);
-
-   // Array in heap
-   int *others = malloc(3*sizeof(int));
-   others[0] = 9;
-   others[1] = 10;
-   others[20] = 8;
-   printf("another %i %i\n",others[0],
-                            others[1]);
-   printf("another %i %i\n",*others,
-                            *(others+1));
-   free(others);
+   char myname[10];
+   myname[0] = 'd';
+   myname[1] = 'a';
+   myname[2] = 'v';
+   myname[3] = 'e';
+   myname[4] = NULL;
+   myname[4] = (char)0;
+   myname[4] = '\0';
+   //badbadbad myname[4] = '0'
+   printf("my name is %s",myname);
 }
 
 
