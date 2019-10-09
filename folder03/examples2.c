aecd7ef9e89ee8ee04fd6429a883983c78b28b62
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-10-09 10:29:19 -0500

    Scripted auto-commit on change (2019-10-09 10:29:19) by gitwatch.sh

--- folder03/examples2.c
+++ folder03/examples2.c
@@ -7,9 +7,9 @@ int main() {
    myname[1] = 'a';
    myname[2] = 'v';
    myname[3] = 'e';
-   myname[4] = (char)NULL;
-   myname[4] = (char)0;
-   myname[4] = '\0';
+   /* myname[4] = (char)NULL; */
+   /* myname[4] = (char)0; */
+   /* myname[4] = '\0'; */
    //badbadbad myname[4] = '0'
    printf("my name is %s\n",myname);
 }
