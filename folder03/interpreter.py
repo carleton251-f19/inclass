8ee413827f96002a673954258bcedfc4875c6396
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-10-11 10:16:44 -0500

    Scripted auto-commit on change (2019-10-11 10:16:44) by gitwatch.sh

--- folder03/interpreter.py
+++ folder03/interpreter.py
@@ -1,12 +1,9 @@
 inpfile = open('myprog.greet','r')
-outfile = open('myprog.bash','w')
 
 for line in inpfile:
-    (command, number) = line.split()
-    outfile.write('for i in {1..' + \
-                  str(number) + '}\n')
-    outfile.write('do\n')
-    outfile.write('  echo "' + command + '"\n')
-    outfile.write('done\n')
+    (command, numberstr) = line.split()
+    number = int(number)
+    for i in range(number):
+        print(command)
+
 inpfile.close()
-outfile.close()
