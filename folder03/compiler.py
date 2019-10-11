582056c763f06985da2a53f20c8bd45b561208cd
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-10-11 10:12:18 -0500

    Scripted auto-commit on change (2019-10-11 10:12:18) by gitwatch.sh

--- folder03/compiler.py
+++ folder03/compiler.py
@@ -1,5 +1,5 @@
-inpfile = open('myprog.greet')
-outfile = open('myprog.bash')
+inpfile = open('myprog.greet','r')
+outfile = open('myprog.bash','w')
 
 for line in inpfile:
     (command, number) = line.split()
