e295bbd1a9cb089ee7aaec76fbe9c23928811d4d
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-11-11 10:23:50 -0600

    Scripted auto-commit on change (2019-11-11 10:23:50) by gitwatch.sh

--- scoping/FunctionParams.java
+++ scoping/FunctionParams.java
@@ -1,16 +1,22 @@
-interface Function {
-   public int f(int x, int y);
+// interface Function {
+//    public int f(int x, int y);
+// }
+
+private class AdderThing { // implements Function {
+   public int f(int x, int y) {
+      return x + y;
+   }
 }
 
 public class FunctionParams {
 
-   public static void evaluate(    f , int x, int y) {
+   public static void evaluate(AdderThing f , int x, int y) {
       System.out.println(f(x,y));
    }
 
    public static void main(String[] args) {
 
-
+      AdderThing add = new AdderThing();
       evaluate(add, 5, 3);
    }
 
