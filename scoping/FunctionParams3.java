db51253328458cd1a7eaf0b899df06ecf6f2c3b3
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-11-11 12:07:52 -0600

    Scripted auto-commit on change (2019-11-11 12:07:52) by gitwatch.sh

--- scoping/FunctionParams3.java
+++ scoping/FunctionParams3.java
@@ -2,13 +2,13 @@ import java.util.function.BiFunction;
 
 public class FunctionParams3 {
 
-   public static void evaluate(BiFunction fobj, int x, int y) {
+   public static void evaluate(BiFunction<Integer,Integer> fobj, int x, int y) {
       System.out.println(fobj.f(x,y));
    }
 
    public static void main(String[] args) {
 
-      BiFunction add = (x,y) -> x + y;
+      BiFunction<Integer,Integer> add = (x,y) -> x + y;
       evaluate(add, 5, 3);
    }
 
