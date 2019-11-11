2cf1fb68840bb4823a85f428fb56a95682b25fd4
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-11-11 10:31:09 -0600

    Scripted auto-commit on change (2019-11-11 10:31:09) by gitwatch.sh

---
+++ scoping/FunctionParams3.java
@@ -0,0 +1,21 @@
+interface Function {
+   public int f(int x, int y);
+}
+
+public class FunctionParams2 {
+
+   public static void evaluate(Function fobj, int x, int y) {
+      System.out.println(fobj.f(x,y));
+   }
+
+   public static void main(String[] args) {
+
+      Function add = new Function() {
+         public int f(int x, int y) {
+            return x + y;
+         }
+      };
+      evaluate(add, 5, 3);
+   }
+
+}
