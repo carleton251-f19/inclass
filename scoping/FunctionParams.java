5b4ed24b2e01b920a737cc3ce17f623a157b43f2
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-11-11 10:19:58 -0600

    Scripted auto-commit on change (2019-11-11 10:19:58) by gitwatch.sh

--- scoping/FunctionParams.java
+++ scoping/FunctionParams.java
@@ -1,3 +1,7 @@
+interface Function {
+   public int f(int x, int y);
+}
+
 public class FunctionParams {
 
    public static void evaluate(    f , int x, int y) {
@@ -5,6 +9,8 @@ public class FunctionParams {
    }
 
    public static void main(String[] args) {
+
+
       evaluate(add, 5, 3);
    }
 
