045024b2b0f08ade768c0e7ef83f33b37b21f920
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-11-11 10:24:51 -0600

    Scripted auto-commit on change (2019-11-11 10:24:51) by gitwatch.sh

--- scoping/FunctionParams.java
+++ scoping/FunctionParams.java
@@ -10,8 +10,8 @@ class AdderThing { // implements Function {
 
 public class FunctionParams {
 
-   public static void evaluate(AdderThing f , int x, int y) {
-      System.out.println(f(x,y));
+   public static void evaluate(AdderThing fobj, int x, int y) {
+      System.out.println(fobj.f(x,y));
    }
 
    public static void main(String[] args) {
