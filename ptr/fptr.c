7b6e27eddd774079ecf90446d8b547119e85a27d
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2019-11-13 10:01:59 -0600

    Scripted auto-commit on change (2019-11-13 10:01:59) by gitwatch.sh

--- ptr/fptr.c
+++ ptr/fptr.c
@@ -4,10 +4,17 @@ int add(int x, int y) {
    return x + y;
 }
 
+void printme() {
+   printf("Just me\n");
+}
+
 int doit(int (*f)(int,int), int x, int y) {
    return (*f)(x,y);
 }
 
+int doitnone(void (*f)(), int x, int y) {
+   (*f)();
+}
 
 int main() {
    printf("%i\n", doit(add, 3, 5));
